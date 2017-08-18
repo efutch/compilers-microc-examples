005e2014e6c53dd44a8038eb06fa9c28662c5fa1
Egdares Futch <efutch@gmail.com>
2017-08-17 23:59:11 -0600

    Create test3.c

---
+++ test3.c
@@ -0,0 +1,77 @@
+int main(int a, int b, int c) 
+{
+  char d,e[10];
+  int f;
+  f=8;
+  d=30;
+  while (f)
+  {
+   e[f]=d;
+   d=d+3;
+  writeint(e[f]);
+  puts(10);
+   f=f-1;
+
+  }
+/*
+  if (1) 
+  a=1;
+  b=2;
+  if (0) 
+  c=3;
+  else
+  d=a+b+c;
+
+  writeint(a);
+  puts(10);
+  writeint(b);
+  puts(10);
+  writeint(c);
+  puts(10);
+  writeint(d);
+  puts(10);
+
+
+  f=0;
+
+  e[f]=f;
+  e[e[f]]=5;
+
+  f=e[4];
+  f=e[f];
+
+  puts(65);
+  writeint(e[f]);
+  f=f+1;
+
+*/
+
+/*  d=4;
+  e=f;
+  g=10;
+  
+  e[0] = a;
+  e[3] = d;
+  f= e[5];
+
+*/
+/*  A=1;
+  B=2;
+  C=3;
+  writeint(33);
+  writeint(A+B*C);
+  writeint(A);
+  puts(10);
+  A=A+1;
+  writeint(A);
+  puts(65);
+  A=A+1;
+  A=A[2];
+  efd[3]=5;
+
+  A=efd[2];
+
+  puts('A'+'G');
+  puts('\n');
+  puts('C');*/
+}
