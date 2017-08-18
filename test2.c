7679b37c4d09f3cd4b4d926ab41a59a4cda2cff9
Egdares Futch <efutch@gmail.com>
2017-08-17 23:57:57 -0600

    Create test2.c

---
+++ test2.c
@@ -0,0 +1,34 @@
+
+int main(int a, int b, int c) 
+{
+  char d,e;
+  int f,g;
+  
+  a=1;
+  b=2;
+  c=3;
+  d=4;
+  e=f;
+  g=10;
+
+
+/*  A=1;
+  B=2;
+  C=3;
+  writeint(33);
+  writeint(A+B*C);
+  writeint(A);
+  puts(10);
+  A=A+1;
+  writeint(A);
+  puts(65);
+  A=A+1;
+  A=A[2];
+  efd[3]=5;
+
+  A=efd[2];
+
+  puts('A'+'G');
+  puts('\n');
+  puts('C');*/
+}
